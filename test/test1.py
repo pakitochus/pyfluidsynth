281e886bdaa7d4a4b60d91765460045f575bb665
pakitochus <pakitochus@gmail.com>
2015-02-20 20:25:10 +0100

    Restoring old test1.py from the original branch

--- test/test1.py
+++ test/test1.py
@@ -1,22 +1,19 @@
 import time
 import fluidsynth
 
-fs = fluidsynth.Synth(polyphony=128, channels=16,gain=1)
-fs.start(driver="alsa")
+fs = fluidsynth.Synth()
+fs.start()
 ## Your installation of FluidSynth may require a different driver.
 ## Use something like:
 # fs.start(driver="pulseaudio")
 
 sfid = fs.sfload("example.sf2")
-
 fs.program_select(0, sfid, 0, 0)
+
 fs.noteon(0, 60, 30)
 fs.noteon(0, 67, 30)
 fs.noteon(0, 76, 30)
 
-fs.get_instrument_list(sfid)
-
-print fs.count_active_voices()
 time.sleep(1.0)
 
 fs.noteoff(0, 60)
