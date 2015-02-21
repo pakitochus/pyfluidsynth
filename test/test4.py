3f199205626ba9be3dec43386e8bb16cac15aa5d
pakitochus <pakitochus@gmail.com>
2015-02-21 15:51:42 +0100

    Updated the test4 with new test of the functions

--- test/test4.py
+++ test/test4.py
@@ -1,35 +1,54 @@
+"""
+This test demonstrated the new functions and options implemented
+in the class Synth.
+"""
 import time
 import fluidsynth
 
-fs = fluidsynth.Synth(polyphony=128, channels=16)
+# Create and start the synthesizer with the selected options.
+fs = fluidsynth.Synth(gain=0.1, polyphony=64, channels=16)
 fs.start(driver="pulseaudio")
-## Your installation of FluidSynth may require a different driver.
-## Use something like:
-# fs.start(driver="pulseaudio") or fs.start(driver="alsa")
 
-sfid = fs.sfload("/usr/share/sounds/sf2/TimGM6mb.sf2")
+# Test the get_gain and set a new gain:
+print "Current gain: " + str(fs.get_gain())
+fs.set_gain(0.3)
+print "Updated gain: " + str(fs.get_gain())
+
+# Load a soundfont.
+sfid = fs.sfload("/usr/share/sounds/sf2/FluidR3_GM.sf2")
 
 preset = 19
 bank = 0
+channel = 0
 
-fs.program_select(0, sfid, bank, preset)
+# Select the program and play a chord.
+fs.program_select(channel, sfid, bank, preset)
 fs.noteon(0, 60, 80)
 fs.noteon(0, 67, 80)
 fs.noteon(0, 76, 80)
 
-insts = fs.get_instrument_list(sfid)
+# This way we check the information of the instrument loaded in a certain channel
+information = fs.get_channel_info(channel)
+bank = information.bank
+preset = information.program
 
+# We present the name of the instrument loaded using the instrument list
+insts = fs.get_instrument_list(sfid)
 print "Playing " + insts[str(bank).zfill(3) + '-' + str(preset).zfill(3)]
 
+# Count the number of active voices
 print "Active voices: " + str(fs.count_active_voices())
+
 time.sleep(1.0)
 
+# Stop the active chord
 fs.noteoff(0, 60)
 fs.noteoff(0, 67)
 fs.noteoff(0, 76)
 
 time.sleep(1.0)
 
+# Print the nubmer of active voices (after stopping the notes)
 print "Active voices: " + str(fs.count_active_voices())
 
 fs.delete()
